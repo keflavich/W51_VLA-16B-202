360fb57b0b1c82204ea3b4bffac99bfadcf2d2d7
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-10-26 13:58:42 -0600

    split up "full" and "wide continuum"

--- reduction/calibration_SB1_wide_continuum.py
+++ reduction/calibration_SB1_wide_continuum.py
@@ -4,50 +4,50 @@ https://casaguides.nrao.edu/index.php/EVLA_high_frequency_Spectral_Line_tutorial
 """
 
 vis = '16B-202.sb32532587.eb32875589.57663.07622001157.ms'
-full_vis = vis
+cont_vis = '16B-202.width8.continuum.ms'
 refant = 'ea14'
 fluxcal = '0137+331=3C48'
 phasecal = 'J1922+1530'
 source = 'W51e2w,W51 North'
 spwrange = ""
-spwrange = '2,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,22,23,24,25,26,28,29,30,31,32,34,35,36,37,39,42,44,45,47,48,49,50,51,52,53,54,55,56,57,58,59,60,62,63,64'
+contspw = '2,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,22,23,24,25,26,28,29,30,31,32,34,35,36,37,39,42,44,45,47,48,49,50,51,52,53,54,55,56,57,58,59,60,62,63,64'
 
-#split(vis=vis, outputvis=full_vis, datacolumn='data', spw='2,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19',
-#      width=8)
-flagdata(vis=full_vis, mode='unflag')
+split(vis=vis, outputvis=cont_vis, datacolumn='data', spw=contspw,
+      width=8)
+flagdata(vis=cont_vis, mode='unflag')
 # flag 1st scan (non-science)
-flagdata(vis=full_vis, flagbackup=T, mode='manual', scan='1')
+flagdata(vis=cont_vis, flagbackup=T, mode='manual', scan='1')
 #quack start of scans
-flagdata(vis=full_vis, mode='quack', quackinterval=10.0, quackmode='beg')
+flagdata(vis=cont_vis, mode='quack', quackinterval=10.0, quackmode='beg')
 # flag pre-pointing obs
-flagdata(vis=full_vis, mode='manual', timerange='01:50:46.0~01:51:44.0')
+flagdata(vis=cont_vis, mode='manual', timerange='01:50:46.0~01:51:44.0')
 
-setjy_dict = setjy(vis=full_vis, field=fluxcal, scalebychan=True, model='3C48_C.im',
+setjy_dict = setjy(vis=cont_vis, field=fluxcal, scalebychan=True, model='3C48_C.im',
                    usescratch=False, standard='Perley-Butler 2013')
 print('setjy: ', setjy_dict)
 
-rmtables(['cal_all_spws.G0','cal_all_spws.K0','cal_all_spws.antpos','cal_all_spws.gaincurve','cal_all_spws.G1inf'])
+rmtables(['cal_cont_spws.G0','cal_cont_spws.K0','cal_cont_spws.antpos','cal_cont_spws.gaincurve','cal_cont_spws.G1inf'])
 # no antenna offsets found....
-# gencal(vis=full_vis,caltable='cal_all_spws.antpos',caltype='antpos',antenna='')
-gencal(vis=full_vis,caltable='cal_all_spws.gaincurve',caltype='gc')
-gencal(vis=full_vis,caltable='cal_all_spws.requantizer',caltype='rq')
+# gencal(vis=cont_vis,caltable='cal_cont_spws.antpos',caltype='antpos',antenna='')
+gencal(vis=cont_vis,caltable='cal_cont_spws.gaincurve',caltype='gc')
+gencal(vis=cont_vis,caltable='cal_cont_spws.requantizer',caltype='rq')
 
 # ea13 is mostly missing, ea21 is partly missing, ea26 is mostly missing
 # (these antennae are on the same arm)
 # ea27 is mostly missing
-gaincal(vis=full_vis,caltable='cal_all_spws.G0', field=fluxcal,spw=spwrange,
-        gaintable=['cal_all_spws.gaincurve'],
+gaincal(vis=cont_vis,caltable='cal_cont_spws.G0', field=fluxcal,spw=spwrange,
+        gaintable=['cal_cont_spws.gaincurve'],
         gaintype='G', refant=refant, calmode='p', solint='20s', minsnr=2.0,
         combine='scan')
 # add in phasecal, so we can use phasecal for delaycal
-gaincal(vis=full_vis,caltable='cal_all_spws.G0',
-        gaintable=['cal_all_spws.gaincurve'],
+gaincal(vis=cont_vis,caltable='cal_cont_spws.G0',
+        gaintable=['cal_cont_spws.gaincurve'],
         field=phasecal,refant=refant,solnorm=False,
         spw=spwrange,
         solint='10s',gaintype='G',calmode='p',append=True)
 
-gaincal(vis=full_vis,caltable='cal_all_spws.K0',
-        gaintable=['cal_all_spws.gaincurve','cal_all_spws.G0'],
+gaincal(vis=cont_vis,caltable='cal_cont_spws.K0',
+        gaintable=['cal_cont_spws.gaincurve','cal_cont_spws.G0'],
         field=phasecal, # previously used fluxcal, but we want brighter
         spw=spwrange, gaintype='K', refant=refant, combine='scan',
         solint='inf', minsnr=2)
@@ -55,9 +55,9 @@ gaincal(vis=full_vis,caltable='cal_all_spws.K0',
 # even with very permissive inputs, bandtype='B' results in major flagging
 # ea13 is bad again, ea21 ea26 ea28 have no solutions, ea27 is missing some spws
 # using the phasecal, this looks a lot better
-bandpass(vis=full_vis,caltable='cal_all_spws.B0',
-         gaintable=['cal_all_spws.gaincurve',
-                    'cal_all_spws.G0','cal_all_spws.K0'],
+bandpass(vis=cont_vis,caltable='cal_cont_spws.B0',
+         gaintable=['cal_cont_spws.gaincurve',
+                    'cal_cont_spws.G0','cal_cont_spws.K0'],
          field=phasecal,refant=refant,solnorm=False,
          fillgaps=4,
          minsnr=2,
@@ -69,36 +69,36 @@ bandpass(vis=full_vis,caltable='cal_all_spws.B0',
 # ea09 missing data
 # ea13, ea21, ea26, ea27, ea28  totally flagged out
 # ea15, ea17 mostly flagged out
-gaincal(vis=full_vis,caltable='cal_all_spws.G1int',
-        gaintable=['cal_all_spws.gaincurve',
-                   'cal_all_spws.K0','cal_all_spws.B0'],
+gaincal(vis=cont_vis,caltable='cal_cont_spws.G1int',
+        gaintable=['cal_cont_spws.gaincurve',
+                   'cal_cont_spws.K0','cal_cont_spws.B0'],
         field=fluxcal,refant=refant,solnorm=False,
         spw=spwrange,
         solint='20s',gaintype='G',calmode='p')
 
 # phasecal: few solutions flagged (~1/34)
-gaincal(vis=full_vis,caltable='cal_all_spws.G1int',
-        gaintable=['cal_all_spws.gaincurve',
-                   'cal_all_spws.K0','cal_all_spws.B0'],
+gaincal(vis=cont_vis,caltable='cal_cont_spws.G1int',
+        gaintable=['cal_cont_spws.gaincurve',
+                   'cal_cont_spws.K0','cal_cont_spws.B0'],
         field=phasecal,refant=refant,solnorm=False,
         spw=spwrange,
         solint='int',gaintype='G',calmode='p',append=True)
 
 
 # phasecal, inf... no complaints
-gaincal(vis=full_vis,caltable='cal_all_spws.G1inf',
-        gaintable=['cal_all_spws.gaincurve',
-                   'cal_all_spws.K0','cal_all_spws.B0'],
+gaincal(vis=cont_vis,caltable='cal_cont_spws.G1inf',
+        gaintable=['cal_cont_spws.gaincurve',
+                   'cal_cont_spws.K0','cal_cont_spws.B0'],
         field=phasecal,refant=refant,solnorm=F,
         spw=spwrange,
         solint='inf',gaintype='G',calmode='p')
 
 # fluxcal inf: only one complaint of 34.  Huh.
-gaincal(vis=full_vis, caltable='cal_all_spws.G2',
-        gaintable=['cal_all_spws.gaincurve',
-                   'cal_all_spws.K0',
-                   'cal_all_spws.B0',
-                   'cal_all_spws.G1int'],
+gaincal(vis=cont_vis, caltable='cal_cont_spws.G2',
+        gaintable=['cal_cont_spws.gaincurve',
+                   'cal_cont_spws.K0',
+                   'cal_cont_spws.B0',
+                   'cal_cont_spws.G1int'],
         gainfield=['','',phasecal,phasecal,fluxcal], # changed from fluxcal->phasecal b/c we changed which is being used
         interp=['','','nearest','nearest','nearest'],
         field=fluxcal,refant=refant,solnorm=F,
@@ -106,11 +106,11 @@ gaincal(vis=full_vis, caltable='cal_all_spws.G2',
         solint='inf',combine='scan',gaintype='G',calmode='a')
 
 # mostly minor?  But it looks like some antennae may have been flagged out
-gaincal(vis=full_vis, caltable='cal_all_spws.G2',
-        gaintable=['cal_all_spws.gaincurve',
-                   'cal_all_spws.K0',
-                   'cal_all_spws.B0',
-                   'cal_all_spws.G1int'],
+gaincal(vis=cont_vis, caltable='cal_cont_spws.G2',
+        gaintable=['cal_cont_spws.gaincurve',
+                   'cal_cont_spws.K0',
+                   'cal_cont_spws.B0',
+                   'cal_cont_spws.G1int'],
         gainfield=['','',phasecal,phasecal,phasecal], # changed from fluxcal->phasecal b/c we changed which is being used
         interp=['','','nearest','nearest','nearest'],
         field=phasecal,refant=refant,solnorm=F,
@@ -118,53 +118,53 @@ gaincal(vis=full_vis, caltable='cal_all_spws.G2',
         solint='inf',gaintype='G',calmode='a',append=True)
 
 
-gaincal(vis=full_vis, caltable='cal_all_spws.G3',
-        gaintable=['cal_all_spws.gaincurve',
-                   'cal_all_spws.K0','cal_all_spws.B0','cal_all_spws.G1int'],
+gaincal(vis=cont_vis, caltable='cal_cont_spws.G3',
+        gaintable=['cal_cont_spws.gaincurve',
+                   'cal_cont_spws.K0','cal_cont_spws.B0','cal_cont_spws.G1int'],
         gainfield=['','',fluxcal,fluxcal,fluxcal],
         interp=['','','nearest','nearest','nearest'],
         field=fluxcal,refant=refant,solnorm=F,
         spw=spwrange,
         solint='inf',combine='scan',gaintype='G',calmode='a')
 #
-gaincal(vis=full_vis, caltable='cal_all_spws.G3',
-        gaintable=['cal_all_spws.gaincurve',
-                   'cal_all_spws.K0','cal_all_spws.B0','cal_all_spws.G1int'],
+gaincal(vis=cont_vis, caltable='cal_cont_spws.G3',
+        gaintable=['cal_cont_spws.gaincurve',
+                   'cal_cont_spws.K0','cal_cont_spws.B0','cal_cont_spws.G1int'],
         gainfield=['','',fluxcal,fluxcal,phasecal],
         interp=['','','nearest','nearest','nearest'],
         field=phasecal,refant=refant,solnorm=F,
         spw=spwrange,
         solint='inf',combine='scan',gaintype='G',calmode='a',append=True)
 
-myflux = fluxscale(vis=full_vis,caltable='cal_all_spws.G3',
-                   fluxtable='cal_all_spws.F3inc',reference=fluxcal,transfer=phasecal,
+myflux = fluxscale(vis=cont_vis,caltable='cal_cont_spws.G3',
+                   fluxtable='cal_cont_spws.F3inc',reference=fluxcal,transfer=phasecal,
                    incremental=True)
 
 
 # g1int, the flux cal's self-cal, flags out 50% of the data
 # maybe we don't care, since we're doing literally nothing with the flux cal data
 # now....
-applycal(vis=full_vis,field=fluxcal,
-         gaintable=['cal_all_spws.gaincurve','cal_all_spws.K0',
-                    'cal_all_spws.B0','cal_all_spws.G1int','cal_all_spws.G2'],
+applycal(vis=cont_vis,field=fluxcal,
+         gaintable=['cal_cont_spws.gaincurve','cal_cont_spws.K0',
+                    'cal_cont_spws.B0','cal_cont_spws.G1int','cal_cont_spws.G2'],
          gainfield=['','','','',fluxcal,fluxcal],
          interp=['','','nearest','nearest','nearest','nearest'],
          parang=False,calwt=False)
 
 # G2 flags out 40%.  40% were already flagged for no clear reason.
-applycal(vis=full_vis,field=phasecal,
-         gaintable=['cal_all_spws.gaincurve','cal_all_spws.K0',
-                    'cal_all_spws.B0','cal_all_spws.G1int','cal_all_spws.G2',
-                    'cal_all_spws.F3inc'],
+applycal(vis=cont_vis,field=phasecal,
+         gaintable=['cal_cont_spws.gaincurve','cal_cont_spws.K0',
+                    'cal_cont_spws.B0','cal_cont_spws.G1int','cal_cont_spws.G2',
+                    'cal_cont_spws.F3inc'],
          gainfield=['','','','',phasecal,phasecal,phasecal],
          interp=['','','nearest','nearest','nearest','nearest',''],
          parang=False,calwt=False)
 
 # again, G2 is the main culprit
-applycal(vis=full_vis,field=source,
-         gaintable=['cal_all_spws.gaincurve','cal_all_spws.K0',
-                    'cal_all_spws.B0','cal_all_spws.G1inf','cal_all_spws.G2',
-                    'cal_all_spws.F3inc'],
+applycal(vis=cont_vis,field=source,
+         gaintable=['cal_cont_spws.gaincurve','cal_cont_spws.K0',
+                    'cal_cont_spws.B0','cal_cont_spws.G1inf','cal_cont_spws.G2',
+                    'cal_cont_spws.F3inc'],
          gainfield=['','','','',phasecal,phasecal,phasecal],
          interp=['','','nearest','nearest','linear','linear',''],
          parang=False,calwt=False)
@@ -172,8 +172,8 @@ applycal(vis=full_vis,field=source,
 
 
 
-imagename = 'W51e2w_QbandAarray_all_spws_raw_continuum_cal_dirty'
-clean(vis=full_vis,
+imagename = 'W51e2w_QbandAarray_cont_spws_raw_continuum_cal_dirty'
+clean(vis=cont_vis,
       imagename=imagename,
       field='W51e2w',
       spw=spwrange,
@@ -183,8 +183,8 @@ clean(vis=full_vis,
 exportfits(imagename+".image", imagename+".image.fits", overwrite=True, dropdeg=True)
 
 
-imagename = 'W51e2w_QbandAarray_all_spws_raw_continuum_cal_clean'
-clean(vis=full_vis,
+imagename = 'W51e2w_QbandAarray_cont_spws_raw_continuum_cal_clean'
+clean(vis=cont_vis,
       imagename=imagename,
       field='W51e2w',
       spw=spwrange,
@@ -196,8 +196,8 @@ exportfits(imagename+".image", imagename+".image.fits", overwrite=True, dropdeg=
 
 
 
-imagename = 'W51North_QbandAarray_all_spws_raw_continuum_cal_dirty'
-clean(vis=full_vis,
+imagename = 'W51North_QbandAarray_cont_spws_raw_continuum_cal_dirty'
+clean(vis=cont_vis,
       imagename=imagename,
       field='W51 North',
       spw=spwrange,
@@ -207,8 +207,8 @@ clean(vis=full_vis,
 exportfits(imagename+".image", imagename+".image.fits", overwrite=True, dropdeg=True)
 
 
-imagename = 'W51North_QbandAarray_all_spws_raw_continuum_cal_clean'
-clean(vis=full_vis,
+imagename = 'W51North_QbandAarray_cont_spws_raw_continuum_cal_clean'
+clean(vis=cont_vis,
       imagename=imagename,
       field='W51 North',
       spw=spwrange,
@@ -223,30 +223,30 @@ exportfits(imagename+".image", imagename+".image.fits", overwrite=True, dropdeg=
 #spw17vis='QbandAarray_spw17_raw_continuum.ms'
 #split(vis=vis, outputvis=spw17vis, datacolumn='data', spw='17')
 #applycal(vis=spw17vis,field=fluxcal,
-#         gaintable=['cal_all_spws.gaincurve','cal_all_spws.K0',
-#                    'cal_all_spws.B0','cal_all_spws.G1int','cal_all_spws.G2'],
+#         gaintable=['cal_cont_spws.gaincurve','cal_cont_spws.K0',
+#                    'cal_cont_spws.B0','cal_cont_spws.G1int','cal_cont_spws.G2'],
 #         gainfield=['','','','',fluxcal,fluxcal],
 #         interp=['','','nearest','nearest','nearest','nearest'],
 #         parang=False,calwt=False)
 ##
 #applycal(vis=spw17vis,field=phasecal,
-#         gaintable=['cal_all_spws.gaincurve','cal_all_spws.K0',
-#                    'cal_all_spws.B0','cal_all_spws.G1int','cal_all_spws.G2',
-#                    'cal_all_spws.F3inc'],
+#         gaintable=['cal_cont_spws.gaincurve','cal_cont_spws.K0',
+#                    'cal_cont_spws.B0','cal_cont_spws.G1int','cal_cont_spws.G2',
+#                    'cal_cont_spws.F3inc'],
 #         gainfield=['','','','',phasecal,phasecal,phasecal],
 #         interp=['','','nearest','nearest','nearest','nearest',''],
 #         parang=False,calwt=False)
 ##
 #applycal(vis=spw17vis,field=source,
-#         gaintable=['cal_all_spws.gaincurve','cal_all_spws.K0',
-#                    'cal_all_spws.B0','cal_all_spws.G1inf','cal_all_spws.G2',
-#                    'cal_all_spws.F3inc'],
+#         gaintable=['cal_cont_spws.gaincurve','cal_cont_spws.K0',
+#                    'cal_cont_spws.B0','cal_cont_spws.G1inf','cal_cont_spws.G2',
+#                    'cal_cont_spws.F3inc'],
 #         gainfield=['','','','',phasecal,phasecal,phasecal],
 #         interp=['','','nearest','nearest','linear','linear',''],
 #         parang=False,calwt=False)
 #
-#full_vis='QbandAarray_all_spws_raw_continuum.ms'
-#split(vis=full_vis, outputvis='QbandAarray_all_spws_cal_continuum.ms', width=128, datacolumn='corrected', spw='0')
+#cont_vis='QbandAarray_cont_spws_raw_continuum.ms'
+#split(vis=cont_vis, outputvis='QbandAarray_cont_spws_cal_continuum.ms', width=128, datacolumn='corrected', spw='0')
 #spw17vis='QbandAarray_spw17_raw_continuum.ms'
 #split(vis=spw17vis, outputvis='QbandAarray_spw17_cal_continuum.ms', width=1024, datacolumn='corrected', spw='0')
 #
