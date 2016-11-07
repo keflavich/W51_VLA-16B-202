4565f51a546fc43345da55eb7fcbe082e1e03d0a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-11-07 11:03:47 -0700

    alternate fitting approach w/gaussfitter

--- analysis/gaussfit_channels.py
+++ analysis/gaussfit_channels.py
@@ -2,7 +2,7 @@ import numpy as np
 from spectral_cube import SpectralCube
 from astropy.io import fits
 from astropy.modeling import models, fitting
-
+import gaussfitter
 
 cube = SpectralCube.read('../FITS/W51North_QbandAarray_SiO_v=1_velocitycutout_small.fits')
 
@@ -30,6 +30,26 @@ for velo,chan in zip(cube.spectral_axis, cube):
                                    x_stddev=x_stddev,
                                    y_stddev=y_stddev)
 
-        params = fit_p(p_init, xcoords, ycoords, chan)
+        params = fit_p(p_init, xcoords.ravel(), ycoords.ravel(),
+                       chan.value.ravel(), weights=1./rms.value**2)
 
-        gfits[velo] = params
+        # alternative approach
+        params,eparams = gaussfitter.gaussfit(chan.value, error=rms.value,
+                                              return_error=True, vheight=False)
+        # get rid of "height" parameter
+        params = params[1:]
+        eparams = eparams[1:]
+
+
+        gfits[velo] = ([params[0],
+                       xcoords[params[1], params[2]],
+                       ycoords[params[1], params[2]],
+                       params[3] * pixscale,
+                       params[4] * pixscale,
+                       params[5],],
+                       [eparams[0],
+                        eparams[1]*pixscale,
+                        eparams[2]*pixscale,
+                        eparams[3]*pixscale,
+                        eparams[4]*pixscale,
+                        eparams[5]])
