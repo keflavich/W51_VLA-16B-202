2f83aab78399cfd80e731b1743a6aff604c1071c
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-06-13 10:04:44 -0600

    don't repeat concatenation

--- reduction/imaging_continuum_selfcal.py
+++ reduction/imaging_continuum_selfcal.py
@@ -26,12 +26,14 @@ vis = [#'16B-202.sb32532587.eb32875589.57663.07622001157.ms',
 contspw = '2,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,22,23,24,25,26,28,29,30,31,32,34,35,36,37,39,42,44,45,47,48,49,50,51,52,53,54,55,56,57,58,59,60,62,63,64'
 
 for cvis in vis:
-    split(vis=cvis, outputvis="cont_"+cvis, spw=contspw)
+    if not os.path.exists("cont_"+cvis):
+        split(vis=cvis, outputvis="cont_"+cvis, spw=contspw)
 
 cont_vises = ["cont_"+vv for vv in vis]
 cont_vis = 'continuum_concatenated.ms'
 
-concat(vis=cont_vises, concatvis=cont_vis)
+if not os.path.exists(cont_vis):
+    concat(vis=cont_vises, concatvis=cont_vis)
 
 selfcal_vis = cont_vis
 
