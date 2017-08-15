4d1e5bf86d9f27e6bf7108f9c22ef2e092d3a43d
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-08-15 14:55:14 -0700

    updates to selfcal script

--- plot_codes/selfcal_progression.py
+++ plot_codes/selfcal_progression.py
@@ -4,7 +4,11 @@ from astropy import coordinates, units as u
 from astropy.io import fits
 from astropy import wcs
 from astropy.nddata import Cutout2D
+import astropy.visualization
 import pylab as pl
+
+import warnings
+warnings.filterwarnings('ignore', category=wcs.FITSFixedWarning, append=True)
 
 def hide_labels(ax):
     lon = ax.coords['RA']
@@ -20,34 +24,57 @@ def hide_labels(ax):
 
 filename_template = "{source}_QbandAarray_cont_spws_continuum_cal_clean_2terms_robust0_wproj_selfcal{selfcaliter}.image.tt0.pbcor.fits"
 
-coords = {'W51e2': coordinates.SkyCoord('19:23:43.910 +14:30:34.500', frame='fk5',
-                                        unit=(u.hour, u.deg)),
-          'W51_North': coordinates.SkyCoord('19:23:40.000 +14:31:05.000',
-                                            frame='fk5', unit=(u.hour, u.deg))
+coords = {'W51e2w': coordinates.SkyCoord('19:23:43.910 +14:30:34.500', frame='fk5',
+                                         unit=(u.hour, u.deg)),
+          'W51_North': coordinates.SkyCoord('19:23:40.050 +14:31:05.467',
+                                            frame='fk5', unit=(u.hour, u.deg)),
+          'W51d2': coordinates.SkyCoord('19:23:39.818 +14:31:04.799',
+                                        frame='fk5', unit=(u.hour, u.deg))
          }
 
 vmin = -0.0001
-vmax = 0.002
+vmax = {'W51e2w': 0.02,
+        'W51_North': 0.002,
+        'W51d2': 0.02,
+       }
+namelist = [('W51e2w','W51e2w'),
+            ('W51_North','W51_North'),
+            ('W51_North','W51d2'),
+           ]
 
-for sourcename in ("W51e2w", "W51_North"):
-    fig = pl.figure(1)
+for fsourcename, sourcename in namelist:
+    fig = pl.figure(1, figsize=(24,6), dpi=75)
     fig.clf()
     for iternum in range(0,13):
-        fh = fits.open(filename_template.format(source=sourcename,
-                                                selfcaliter=iternum))
+        try:
+            fh = fits.open(filename_template.format(source=fsourcename,
+                                                    selfcaliter=iternum))
+        except FileNotFoundError:
+            print("Did not find "
+                  "{0}".format(filename_template.format(source=fsourcename,
+                                                        selfcaliter=iternum)))
+            continue
 
         mywcs = wcs.WCS(fh[0].header)
         center = coords[sourcename]
-        size = 5*u.arcsec
+        size = 1*u.arcsec
         cutout_im = Cutout2D(fh[0].data, position=center, size=size, wcs=mywcs)
 
-        ax = pl.subplot(7,2,iternum+1)
+        std = astropy.stats.mad_std(fh[0].data[np.isfinite(fh[0].data)])
+        peak = cutout_im.data.max()
+        print("iter{3} Peak = {0}, RMS = {1}, S/N = {2}"
+              .format(peak,
+                      std,
+                      peak/std,
+                      iternum))
+
+        ax = fig.add_subplot(2,7,iternum+1, projection=cutout_im.wcs)
 
         im = ax.imshow(cutout_im.data*1e3, cmap='gray',
                        norm=astropy.visualization.simple_norm(fh[0].data,
                                                               stretch='asinh',
                                                               min_cut=vmin*1e3,
-                                                              max_cut=vmax*1e3,
+                                                              max_cut=vmax[sourcename]*1e3,
                                                               asinh_a=0.001),
                        transform=ax.get_transform(cutout_im.wcs),
                        origin='lower',)
@@ -64,6 +91,8 @@ for sourcename in ("W51e2w", "W51_North"):
     cb = pl.colorbar(mappable=im, cax=fig.add_axes(cax_bbox))
     cb.set_label('mJy/beam')
 
+    fig.subplots_adjust(wspace=0, hspace=0)
+    fig.canvas.draw()
 
     fig.savefig("selfcal_progression_{0}.pdf".format(sourcename),
                 bbox_inches='tight', dpi=300)
