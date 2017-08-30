d030baa6e204910b541c51135f57c8d505438238
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-08-30 14:45:42 -0600

    update flagging commands

--- reduction/flagging_sb2.py
+++ reduction/flagging_sb2.py
@@ -5,6 +5,9 @@ flagdata(vis=vis, antenna='ea05', spw='34~44', mode='manual', action='apply')
 flagdata(vis=vis, antenna='ea25', timerange='16:55:12~17:00:00', mode='manual', action='apply')
 flagdata(vis=vis, antenna='ea07', timerange='15:00:00~15:30:00', mode='manual', action='apply')
 flagdata(vis=vis, antenna='ea04~ea27', spw='18~32', mode='manual', action='apply')
+flagdata(vis=vis, antenna='ea17', spw='45~46GHz', corr='RR', mode='manual', action='apply')
+flagdata(vis=vis, antenna='ea04', spw='45.4~46GHz', corr='RR', mode='manual', action='apply')
+flagdata(vis=vis, antenna='ea24', spw='44.0~44.6GHz', corr='LL', mode='manual', action='apply')
 
 # these are regions where the two polarizations diverge in their poln solutions
 #flagdata(vis=vis, antenna='ea04', polarization='1', spw='2~17', mode='manual', action='apply')
