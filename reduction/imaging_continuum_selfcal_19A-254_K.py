4c743c5d001e097f1d184a550e6d21441ca02f2d
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2021-07-27 17:06:50 -0400

    add k-band clean mask

--- reduction/imaging_continuum_selfcal_19A-254_K.py
+++ reduction/imaging_continuum_selfcal_19A-254_K.py
@@ -24,49 +24,43 @@ vis = [
       ]
 contspw = '11,12,14,16,17,19,20,21,22,23,24,25,26,27,29,30,31,32,33,34,35,36,39,40,45,46,49,51,52,57,58,59,60,61'
 
-for cvis in vis:
-    if not os.path.exists("cont_"+cvis):
-        split(vis=cvis, outputvis="cont_"+cvis, spw=contspw)
 
-cont_vises = ["cont_"+vv for vv in vis]
 cont_vis = 'continuum_concatenated.ms'
 
 if not os.path.exists(cont_vis):
+    for cvis in vis:
+        if not os.path.exists("cont_"+cvis):
+            split(vis=cvis, outputvis="cont_"+cvis, spw=contspw)
+
+    cont_vises = ["cont_"+vv for vv in vis]
+
     assert concat(vis=cont_vises, concatvis=cont_vis)
 
 selfcal_vis = cont_vis
 
-imsize = 7680
+imsize = 10000
 
 mask = None
 
-thresholds = {'W51e2w': (2.5,1.5,1.0,1.0,1.0,0.5,0.25,0.25,0.25,0.25,0.25,0.25),
-              'W51 North': (2.5,1.5,1.0,1.0,1.0,1.0,0.5,0.5,0.5,0.5,0.5,0.5,0.5),
+thresholds = {'W51e2w': (0.7,0.5,0.3,0.2,0.1),
+              'W51 North': (0.4,0.3,0.2,0.1),
              }
 
 for field in field_list:
 
     # must iterate over fields separately because the mask name is being set and reset
-    for iternum, nterms, threshold, caltype, calmode, solint in [(0, 2, '{0} mJy','phase','p', '30s'), # first attempt was 5; too conservative
-                                                                 #(1, 2, '{0} mJy','phase','p', '30s'),
-                                                                 #(2, 2, '{0} mJy','phase','p', '30s'),
-                                                                 #(3, 2, '{0} mJy','phase','p', 'int'),
-                                                                 #(4, 2, '{0} mJy','phase','p', 'int'), # mostly a sanity check
-                                                                 #(5, 2, '{0} mJy','ampphase','ap', '120s'),
-                                                                 #(6, 2, '{0} mJy','ampphase','ap', '120s'),
-                                                                 #(7, 2, '{0} mJy','ampphase','ap', '120s'),
-                                                                 #(8, 2, '{0} mJy','ampphase','ap', '30s'),
-                                                                 #(9, 2, '{0} mJy','ampphase','ap', 'int'),
-                                                                 #(10, 3, '{0} mJy','bandpass', 'ap', 'inf'),
-                                                                 #(11, 3, '{0} mJy','bandpass', 'ap', 'inf'),
-                                                                 #(12, 3, '{0} mJy','bandpass', 'ap', 'inf'),
-                                                                ]:
+    for iternum, nterms, threshold, caltype, calmode, solint, mask in [(0, 2, '{0} mJy','phase','p', 'inf', 'w51e2w_kband_selfcal0_mask.crtf'), # first attempt was 5; too conservative
+                                                                       (1, 2, '{0} mJy','phase','p', 'inf', 'w51e2w_kband_selfcal0_mask.crtf'),
+                                                                       (2, 2, '{0} mJy','phase','p', 'inf', 'w51e2w_kband_selfcal0_mask.crtf'),
+                                                                       (3, 2, '{0} mJy','phase','p', 'int', 'w51e2w_kband_selfcal0_mask.crtf'),
+                                                                       (4, 2, '{0} mJy','phase','p', 'int', 'w51e2w_kband_selfcal0_mask.crtf'),
+                                                                      ]:
         threshold = threshold.format(thresholds[field][iternum])
         field_nospace = field.replace(" ","_")
-        output = myimagebase = imagename = '{0}_KbandAarray_cont_spws_continuum_cal_clean_2terms_robust0_wproj_selfcal{1}'.format(field_nospace, iternum)
+        output = myimagebase = imagename = '{0}_KbandAarray_cont_spws_continuum_cal_clean_2terms_robust0_selfcal{1}'.format(field_nospace, iternum)
 
         if os.path.exists(imagename+".image.tt0"):
-            mask = 'clean_mask_{0}_{1}.mask'.format(iternum, field_nospace)
+            #mask = 'clean_mask_{0}_{1}.mask'.format(iternum, field_nospace)
             print("Skipping {0}".format(imagename))
             continue
 
@@ -87,11 +81,12 @@ for field in field_list:
                weighting='briggs',
                robust=0.0,
                imsize=imsize,
-               cell=['0.02 arcsec'],
+               cell=['0.015 arcsec'],
                threshold=threshold,
+               cyclefactor=3,
                niter=100000,
-               gridder='wproject',
-               wprojplanes=32,
+               gridder='standard',
+               #wprojplanes=32,
                specmode='mfs',
                deconvolver='mtmfs',
                outframe='LSRK',
@@ -117,18 +112,18 @@ for field in field_list:
         applycal(vis=selfcal_vis, field=field, gaintable=[caltable],
                  interp="linear", applymode='calonly', calwt=True)
 
-        cleanimage = myimagebase+'.image.tt0'
-        ia.open(cleanimage)
-        ia.calcmask(mask=cleanimage+" > {0}".format(float(threshold.split()[0])/1000),
-                    name='clean_mask_iter{0}_{1}'.format(iternum, field_nospace))
+        # cleanimage = myimagebase+'.image.tt0'
+        # ia.open(cleanimage)
+        # ia.calcmask(mask=cleanimage+" > {0}".format(float(threshold.split()[0])/1000),
+        #             name='clean_mask_iter{0}_{1}'.format(iternum, field_nospace))
 
-        ia.close()
-        makemask(mode='copy', inpimage=cleanimage,
-                 inpmask=cleanimage+":clean_mask_iter{0}_{1}".format(iternum, field_nospace),
-                 output='clean_mask_{0}_{1}.mask'.format(iternum, field_nospace),
-                 overwrite=True)
-        mask = 'clean_mask_{0}_{1}.mask'.format(iternum, field_nospace)
-        exportfits(mask, mask+'.fits', dropdeg=True, overwrite=True)
+        # ia.close()
+        # makemask(mode='copy', inpimage=cleanimage,
+        #          inpmask=cleanimage+":clean_mask_iter{0}_{1}".format(iternum, field_nospace),
+        #          output='clean_mask_{0}_{1}.mask'.format(iternum, field_nospace),
+        #          overwrite=True)
+        # mask = 'clean_mask_{0}_{1}.mask'.format(iternum, field_nospace)
+        # exportfits(mask, mask+'.fits', dropdeg=True, overwrite=True)
 
         ia.open(myimagebase+".model.tt0")
         stats = ia.statistics()
@@ -139,30 +134,45 @@ for field in field_list:
 
 
 
-# for field in field_list:
-#     field_nospace = field.replace(" ","_")
-#     output = myimagebase = imagename = '{0}_KbandAarray_cont_spws_continuum_cal_clean_2terms_robust0_wproj_selfcal{1}'.format(field_nospace, iternum+1)
-# 
-#     for suffix in ('pb', 'weight', 'sumwt', 'psf', 'model', 'mask',
-#                    'image', 'residual'):
-#         os.system('rm -rf {0}.{1}'.format(output, suffix))
-# 
-#     tclean(vis=selfcal_vis,
-#            imagename=imagename,
-#            field=field,
-#            spw='',
-#            weighting='briggs',
-#            robust=0.0,
-#            imsize=imsize,
-#            cell=['0.02 arcsec'],
-#            threshold=threshold,
-#            niter=10000,
-#            gridder='wproject',
-#            wprojplanes=32,
-#            specmode='mfs',
-#            deconvolver='mtmfs',
-#            outframe='LSRK',
-#            savemodel='modelcolumn',
-#            nterms=2,
-#            selectdata=True)
-#     makefits(myimagebase)
+    # final iteration: smaller pixels
+
+    output = myimagebase = imagename = '{0}_KbandAarray_cont_spws_continuum_cal_clean_2terms_robust0_selfcal{1}_final'.format(field_nospace, iternum)
+
+    if os.path.exists(imagename+".image.tt0"):
+        #mask = 'clean_mask_{0}_{1}.mask'.format(iternum, field_nospace)
+        print("Skipping {0}".format(imagename))
+        continue
+
+    for ttsuffix in ('.tt0', '.tt1', '.tt2'):
+        for suffix in ('pb{tt}', 'weight', 'sumwt{tt}', 'psf{tt}',
+                       'model{tt}', 'mask', 'image{tt}', 'residual{tt}',
+                       'image{tt}.pbcor',
+                       'alpha', ):
+            rmfile = "{0}.{1}".format(output, suffix).format(tt=ttsuffix)
+            if os.path.exists(rmfile):
+                print("Removing {0}".format(rmfile))
+                os.system('rm -rf {0}'.format(rmfile))
+
+    tclean(vis=selfcal_vis,
+           imagename=imagename,
+           field=field,
+           spw='',
+           weighting='briggs',
+           robust=0.0,
+           imsize=15000,
+           cell=['0.01 arcsec'],
+           threshold=threshold,
+           cyclefactor=3,
+           niter=100000,
+           gridder='standard',
+           #wprojplanes=32,
+           specmode='mfs',
+           deconvolver='mtmfs',
+           outframe='LSRK',
+           savemodel='modelcolumn',
+           scales=[0,3,9],
+           nterms=nterms,
+           selectdata=True,
+           mask=mask,
+          )
+    makefits(myimagebase)
